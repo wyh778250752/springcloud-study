b390f26e9749b4fefedf0d31ec8c918cf1cbd574
123winds <778250752@qq.com>
2022-06-15 21:31:52 +0800

    增加了Discovery服务发现

--- eureka-provider9000/src/main/java/com/wuyuhui/eurekaprovider9000/controller/AddressController.java
+++ eureka-provider9000/src/main/java/com/wuyuhui/eurekaprovider9000/controller/AddressController.java
@@ -5,9 +5,12 @@ import com.baomidou.mybatisplus.core.conditions.query.QueryWrapper;
 import com.wuyuhui.eurekaprovider9000.pojo.Address;
 import com.wuyuhui.eurekaprovider9000.pojo.CommonResult;
 import com.wuyuhui.eurekaprovider9000.service.AddressService;
+import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.lang.StringUtils;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Value;
+import org.springframework.cloud.client.ServiceInstance;
+import org.springframework.cloud.client.discovery.DiscoveryClient;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -16,6 +19,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
+@Slf4j
 public class AddressController {
 
     @Autowired
@@ -23,6 +27,9 @@ public class AddressController {
 
     @Value("${server.port}")
     private String serverPort;
+
+    @Autowired
+    private DiscoveryClient discoveryClient;
 
     @GetMapping("/query/{id}")
     public CommonResult<Address> queryAddressById(@PathVariable("id") int id) {
@@ -35,6 +42,23 @@ public class AddressController {
         }else {
             return new CommonResult(444,"查询失败 服务端口:serverPort:" + serverPort,null);
         }
+    }
+
+    @GetMapping("/discovery")
+    public Object discovery() {
+        List<String> services = discoveryClient.getServices();
+        for (String element : services) {
+            log.info("*****element:" + element);
+        }
+
+        List<ServiceInstance> instances = discoveryClient.getInstances("EUREKA-PROVIDER");
+        for (ServiceInstance instance : instances) {
+            log.info(instance.getServiceId()+"\t" +
+                    instance.getHost()+ "\t" +
+                    instance.getPort()+"\t" +
+                    instance.getUri());
+        }
+        return this.discoveryClient;
     }
 
 
